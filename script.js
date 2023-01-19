8cbd23ab2866943fcfae43e1b934e9e473cbc05e
Pmoonesi <parham.moonesi@outlook.com>
2023-01-19 12:47:51 +0330

    add overlay menu

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 // handle scroll to books part
 
 // handle book info overlay
+
+// handle overlay menu
+
+function toggleNav(event) {
+    var nav = document.getElementById("myNav");
+    if (nav.style.width == "100%") {
+        nav.style.width = "0%";
+    } else {
+        nav.style.width = "100%";
+    }
+}
+
+document.getElementById("check").addEventListener("click", toggleNav);
