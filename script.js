fd72b442c98ad4d3e230db33a8f83b73674e61ed
Pmoonesi <parham.moonesi@outlook.com>
2023-01-27 04:35:22 +0330

    fix nav bar and remove js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-// handle overlay menu
+// // handle overlay menu
 
-function toggleNav(event) {
-    var nav = document.getElementById("myNav");
-    if (nav.style.width == "100%") {
-        nav.style.width = "0%";
-    } else {
-        nav.style.width = "100%";
-    }
-}
+// function toggleNav(event) {
+//     var nav = document.getElementById("myNav");
+//     if (nav.style.width == "100%") {
+//         nav.style.width = "0%";
+//     } else {
+//         nav.style.width = "100%";
+//     }
+// }
 
-document.getElementById("check").addEventListener("click", toggleNav);
+// document.getElementById("check").addEventListener("click", toggleNav);
 
-let links = document.getElementById('nav-links').children
-for (const link of links) {
-    link.addEventListener('click', toggleNav)
-}
+// let links = document.getElementById('nav-links').children
+// for (const link of links) {
+//     link.addEventListener('click', toggleNav)
+// }
